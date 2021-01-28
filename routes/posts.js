8d8a726d98148c99ef636b4319b26b05cba66865
Arkaraj <arkaraj2017@gmail.com>
2021-01-28 22:39:33 +0530

    Completed :sparkles:

--- routes/posts.js
+++ routes/posts.js
@@ -7,6 +7,11 @@ router.get('/new', (req, res) => {
     res.render('./posts/new', { article: new Article() })
 });
 
+router.get('/edit/:id', async (req, res) => {
+    const article = await Article.findById(req.params.id)
+    res.render('posts/edit', { article: article })
+});
+
 router.get('/:slug', async (req, res) => {
 
     const article = await Article.findOne({ slug: req.params.slug })
@@ -15,15 +20,11 @@ router.get('/:slug', async (req, res) => {
 
 });
 
-router.get('/edit/:id', async (req, res) => {
-    const article = await Article.findById(req.params.id);
-    res.render('posts/edit', { article: article });
-});
-
 router.post('/', async (req, res) => {
-    const { title, description, content } = req.body;
+    const { title, image, description, content } = req.body;
     let article = new Article({
         title,
+        image,
         description,
         markdown: content
     })
@@ -44,9 +45,10 @@ router.delete('/:id', async (req, res) => {
 })
 
 router.put('/:id', async (req, res) => {
-    const { title, description, content } = req.body;
+    const { title, image, description, content } = req.body;
     let article = await Article.findById(req.params.id);
     article.title = title;
+    article.image = image;
     article.description = description;
     article.markdown = content;
 
--- views/main.js
+++ views/main.js
@@ -1,6 +1,10 @@
 const ls = document.querySelector('#lightToggle');
 const svg = document.querySelectorAll('#modes');
 const bodyTag = document.querySelector('body');
+const codeColor = document.querySelectorAll('code');
+if (!codeColor) {
+    codeColor = 'nope';
+}
 
 ls.addEventListener('click', () => {
     if (bodyTag.classList.contains('dark')) {
@@ -15,8 +19,29 @@ function toggleSvg() {
     svg.forEach((lsi) => {
         if (lsi.classList.contains('hidden')) {
             lsi.classList.remove('hidden')
+            if (codeColor) {
+                codeColor.forEach(clr => {
+                    clr.style.color = '#a5d6ff'
+                })
+
+            } else {
+
+            }
         } else {
             lsi.classList.add('hidden')
+            if (codeColor) {
+                codeColor.forEach(clr => {
+                    clr.style.color = '#e83e8c'
+                })
+            } else {
+
+            }
         }
     });
 }
+
+function auth() {
+    alert('As this is a personal Blog you are not allowed to edit or add this, Keep Reading 😁');
+}
+const consolestyles = ['font-size: 46px', 'color: #757575'].join(';');
+console.log('%cArkaraj', consolestyles);
